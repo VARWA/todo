5fc46f666a0d61a75b51a946a2f8775ee039a554
Богдан Лупанов <60575285+VARWA@users.noreply.github.com>
2023-06-12 02:47:06 +0300

    change screen changes template

--- lib/change_task_screen/change_task_screen_widget.dart
+++ lib/change_task_screen/change_task_screen_widget.dart
@@ -7,7 +7,50 @@ class ChangeTaskScreenWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      body: SizedBox.shrink(),
+      appBar: AppBar(
+        leading: Icon(Icons.close),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: Text('Сохранить'),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20, right: 16, left: 16),
+        child: ListView(
+
+          children: [
+            Column(mainAxisSize: MainAxisSize.min,
+              children: [TextField()],
+            ),
+            SizedBox(height: 28),
+            // DropdownButton(
+            //     items: ['Нет', 'Низкий', 'Высокий'],
+            //     onChanged: (String? newValue) {}),
+            SizedBox(height: 4),
+            Text('Нет'),
+            const SizedBox(height: 16),
+            const Divider(),
+            ListTile(
+              title: Text('Сделать до'),
+              subtitle: Text('2 июня 2021'),
+              trailing: Switch(
+                value: false,
+                onChanged: (bool value) {},
+              ),
+            ),
+            const SizedBox(height: 23),
+            InkWell(
+              child: const ListTile(
+                leading: Icon(Icons.beach_access),
+                title: Text('Удалить'),
+              ),
+              onTap: () {},
+            )
+          ],
+        ),
+      ),
     );
   }
 }
