6a974dc47200e1a2aca5efe798cb7a34c49d7bf4
Богдан Лупанов <60575285+VARWA@users.noreply.github.com>
2023-06-24 05:27:03 +0300

    updated themes

--- lib/themes/src/text_theme.dart
+++ lib/themes/src/text_theme.dart
@@ -3,14 +3,9 @@ import 'package:todo/themes/src/constants.dart';
 
 TextTheme createTextTheme() {
   return const TextTheme(
-    titleLarge: largeTitleTextStyle,
-    // titleMedium: titleTextStyle,
-    displayMedium: titleTextStyle,
-
-    bodyMedium: bodyTextStyle,
-    titleMedium: subheadTextStyle,
-
-    titleSmall: subheadTextStyle,
-    labelLarge: buttonTextStyle,
-  );
+      titleLarge: largeTitleTextStyle,
+      bodyMedium: bodyTextStyle,
+      titleSmall: subheadTextStyle,
+      labelLarge: buttonTextStyle,
+      labelMedium: buttonTextStyle);
 }
