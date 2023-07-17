28dd8992ef2a694520f3c7b544b24f35d3c2d6ce
Богдан Лупанов <60575285+VARWA@users.noreply.github.com>
2023-07-17 18:24:53 +0300

    change analytics options

--- lib/ui/main_screen/widgets/main_app_bar_widget.dart
+++ lib/ui/main_screen/widgets/main_app_bar_widget.dart
@@ -1,8 +1,10 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/generated/locale_keys.g.dart';
+import 'package:todo/src/flavor_mode.dart';
 import 'package:todo/ui/src/form_factor.dart';
 
+import '../../../di/service_locator.dart';
 import '../../../src/themes/src/custom_extension.dart';
 
 class MainAppBarWidget extends StatelessWidget {
@@ -14,6 +16,10 @@ class MainAppBarWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final customColors = Theme.of(context).extension<CustomColors>()!;
     final globalPadding = establishGlobalPadding(context: context);
+    final FlavorMode flavorMode = locator<FlavorMode>();
+    final String postfix =
+        flavorMode.flavorModeValue == FlavorModes.dev ? '(dev)' : '';
+
     return SliverAppBar(
       backgroundColor: customColors.backPrimary,
       pinned: true,
@@ -28,7 +34,7 @@ class MainAppBarWidget extends StatelessWidget {
         ),
         child: FlexibleSpaceBar(
           title: Text(
-            LocaleKeys.myTasks.tr(),
+            LocaleKeys.myTasks.tr() + postfix,
             style: Theme.of(context).textTheme.titleLarge?.copyWith(
                   color: customColors.labelPrimary,
                 ),
